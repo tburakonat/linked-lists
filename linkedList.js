40a54531964775385a3e005a98ab8e83b7519bbf
tburakonat <tburakonat@gmail.com>
2022-10-15 16:51:20 +0200

    added append method

--- linkedList.js
+++ linkedList.js
@@ -1,6 +1,19 @@
+import Node from './node';
 export default class LinkedList {
 	constructor(headNode) {
 		this.HEAD = headNode;
 		this.HEAD.nextNode = null;
+	}
+
+	append(value) {
+		const newNode = new Node(value);
+
+		let temp = this.HEAD;
+
+		while (temp.nextNode != null) {
+			temp = temp.nextNode;
+		}
+
+		temp.nextNode = newNode;
 	}
 }
